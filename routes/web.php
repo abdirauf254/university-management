7367d1cb6e7ad265421415ec7522a85a89b73528
abdirauf254 <abdirauf899@gmail.com>
2026-09-20 10:36:24 +0300

    initial commits

--- routes/web.php
+++ routes/web.php
@@ -17,4 +17,8 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
+Route::get('/test-university-access', function () {
+    return 'University access middleware is working.';
+})->middleware(['auth', 'university']);
+
 require __DIR__.'/auth.php';
